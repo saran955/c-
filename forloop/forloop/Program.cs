d9880cf440d04cee1cd28dd2f33f981b3abf3fba
saran955 <saravananmohanakrishnan@gmail.com>
2019-02-02 19:52:43 +0000

    c#
    c#

---
+++ forloop/forloop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 10; i++)
+
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+
+}
---
+++ passcode/passcode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace passcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = "";
+
+            while (code != "secret")
+            {
+                Console.WriteLine("what is the passcode?");
+                code = Console.ReadLine();
+
+               
+                if (code != "secret")
+                {
+                    Console.WriteLine("not authenticated");
+                }
+            }
+            Console.WriteLine("Authenticate");
+        }
+    }
+}
